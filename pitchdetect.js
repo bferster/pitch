e834da8842b9ce62a128d31eabdaeac5a2b592f2
bferster <bferster@stagetools.com>
2017-12-29 16:42:58 -0500

    mp3 playback

--- pitchdetect.js
+++ pitchdetect.js
@@ -8,31 +8,33 @@ var analyser = null;
 var theBuffer = null;
 var DEBUGCANVAS = null;
 var mediaStreamSource = null;
-var detectorElem, 
-	canvasElem,
-	waveCanvas,
-	pitchElem,
-	noteElem,
-	detuneElem,
-	detuneAmount;
+var	waveCanvas, waveCanvas2, pitchElem;
+var mp3file=null;
 
 window.onload = function() {
 	audioContext = new AudioContext();
 	MAX_SIZE = Math.max(4,Math.floor(audioContext.sampleRate/5000));	// corresponds to a 5kHz signal
-	detectorElem = document.getElementById( "detector" );
-	canvasElem = document.getElementById( "output" );
-	DEBUGCANVAS = document.getElementById( "waveform" );
-	if (DEBUGCANVAS) {
-		waveCanvas = DEBUGCANVAS.getContext("2d");
-		waveCanvas.strokeStyle = "black";
-		waveCanvas.lineWidth = 1;
+	var request = new XMLHttpRequest();
+	request.open("GET", "hello.mp3", true);
+	request.responseType = "arraybuffer";
+	request.onload=function() {
+		audioContext.decodeAudioData( request.response,function(buffer) { 
+	   		theBuffer=buffer;
+			});
 		}
-	pitchElem = document.getElementById( "pitch" );
+	request.send();
+
+	waveCanvas = document.getElementById( "waveform" ).getContext("2d");
+	waveCanvas2 = document.getElementById( "waveform2" ).getContext("2d");
+
+		pitchElem = document.getElementById( "pitch" );
 }
 
-function error() {
-    alert('Stream generation failed.');
-}
+function error() {   alert("Doesn't work on this browser. Try Firefox"); }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// AUDIO SOURCE
+////////////////////////////////////////////////////////////////////////////////////////////
 
 function getUserMedia(dictionary, callback) {
     try {
@@ -51,21 +53,16 @@ function gotStream(stream) {
     analyser=audioContext.createAnalyser();
     analyser.fftSize = 2048;
     mediaStreamSource.connect( analyser );
-    updatePitch();
+	jj=0;
+	for (i=0;i<512;++i)	tbuf[i]=0;
+	rafID=setInterval(updatePitch,1)									// Start timer
 }
 
 function toggleLiveInput() {
-    if (isPlaying) {
-        //stop playing and return
-        sourceNode.stop( 0 );
-        sourceNode = null;
-        analyser = null;
-        isPlaying = false;
-		if (!window.cancelAnimationFrame)
-			window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
-        window.cancelAnimationFrame( rafID );
-    }
-    getUserMedia(
+        isPlaying=false;
+		clearInterval(rafID);											// Clear timer
+
+		getUserMedia(
     	{
             "audio": {
                 "mandatory": {
@@ -80,36 +77,25 @@ function toggleLiveInput() {
 }
 
 function togglePlayback() {
-    if (isPlaying) {
-        //stop playing and return
-        sourceNode.stop( 0 );
-        sourceNode = null;
-        analyser = null;
-        isPlaying = false;
-		if (!window.cancelAnimationFrame)
-			window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
-        window.cancelAnimationFrame( rafID );
-        return "start";
-    }
 
-    sourceNode = audioContext.createBufferSource();
-    sourceNode.buffer = theBuffer;
-    sourceNode.loop = true;
+ 	clearInterval(rafID);											
+  	kk=jj=0;
+	for (i=0;i<512;++i)	tbuf[i]=0;
+	sourceNode=audioContext.createBufferSource();
+    sourceNode.buffer=theBuffer;
+    analyser=audioContext.createAnalyser();
+    analyser.fftSize=2048;
+    sourceNode.connect(analyser);
+    analyser.connect(audioContext.destination);
+    sourceNode.start(0);
+ 	rafID=setInterval(updatePitch2,1)									
+ }
 
-    analyser = audioContext.createAnalyser();
-    analyser.fftSize = 2048;
-    sourceNode.connect( analyser );
-    analyser.connect( audioContext.destination );
-    sourceNode.start( 0 );
-    isPlaying = true;
-    isLiveInput = false;
-    updatePitch();
-
-    return "stop";
-}
+////////////////////////////////////////////////////////////////////////////////////////////
+// FIND/SHOW PITCH
+////////////////////////////////////////////////////////////////////////////////////////////
 
 var rafID = null;
-var tracks = null;
 var buflen = 1024;
 var buf = new Float32Array( buflen );
 var tbuf = new Float32Array( buflen );
@@ -117,21 +103,22 @@ var tbuf = new Float32Array( buflen );
 var MIN_SAMPLES = 0;  // will be initialized when AudioContext is created.
 var GOOD_ENOUGH_CORRELATION = 0.9; // this is the "bar" for how close a correlation needs to be
 
-function autoCorrelate( buf, sampleRate ) {
+
+function autoCorrelate(buf,sampleRate) {
 	var SIZE = buf.length;
 	var MAX_SAMPLES = Math.floor(SIZE/2);
 	var best_offset = -1;
 	var best_correlation = 0;
-	var rms = 0;
-	var foundGoodCorrelation = false;
-	var correlations = new Array(MAX_SAMPLES);
+	var rms=0;
+	var foundGoodCorrelation=false;
+	var correlations=new Array(MAX_SAMPLES);
 
 	for (var i=0;i<SIZE;i++) {
-		var val = buf[i];
-		rms += val*val;
-	}
-	rms = Math.sqrt(rms/SIZE);
-	if (rms<0.01) // not enough signal
+		var val=buf[i];
+		rms+=val*val;
+		}
+	rms=Math.sqrt(rms/SIZE);
+	if (rms < 0.01) 								// not enough signal
 		return -1;
 
 	var lastCorrelation=1;
@@ -170,58 +157,61 @@ function autoCorrelate( buf, sampleRate ) {
 	}
 	return -1;
 }
+	
+	function updatePitch() 
+	{
+		var i,y;
+		analyser.getFloatTimeDomainData(buf);
+		var ac=autoCorrelate(buf,audioContext.sampleRate/2);
+		waveCanvas.clearRect(0,0,512,256);
+		waveCanvas.strokeStyle="#000099";
+		waveCanvas.lineWidth=6;
+		waveCanvas.lineCap="round";
+		waveCanvas.beginPath();
+		waveCanvas.moveTo(0,256-tbuf[1]/2);
+		for (var i=1;i<512;i++) {
+			if (tbuf[i] == 0)  break;
+			y=tbuf[i]-0+((tbuf[i+1]-tbuf[i])/2);					// Mid y										
+			waveCanvas.quadraticCurveTo(i*4,256-tbuf[i]/2,i*4+2,256-y/2);
+			}
+		waveCanvas.stroke();
 
-function updatePitch( time ) {
-	var cycles = new Array;
-	analyser.getFloatTimeDomainData( buf );
-//	analyser.getFloatFrequencyData(tbuf);	
-
-	var ac = autoCorrelate( buf, audioContext.sampleRate );
-	waveCanvas.clearRect(0,0,512,256);
-	waveCanvas.strokeStyle = "red";
-	waveCanvas.beginPath();
-	waveCanvas.moveTo(0,0);
-	waveCanvas.lineTo(0,256);
-	waveCanvas.moveTo(128,0);
-	waveCanvas.lineTo(128,256);
-	waveCanvas.moveTo(256,0);
-	waveCanvas.lineTo(256,256);
-	waveCanvas.moveTo(384,0);
-	waveCanvas.lineTo(384,256);
-	waveCanvas.moveTo(512,0);
-	waveCanvas.lineTo(512,256);
-	waveCanvas.stroke();
-	waveCanvas.strokeStyle = "#777";
-	waveCanvas.beginPath();
-	waveCanvas.moveTo(0,buf[0]);
-	for (var i=1;i<128;i++) {
-		waveCanvas.lineTo(i*4,(tbuf[i])/4);
-	}
-	waveCanvas.stroke();
-
- 	if (ac != -1) {
-trace(kk)		
-		
-		pitch = ac;
-		pitchElem.innerText = Math.round( pitch ) ;
-if (pitch < 500) {
-tbuf[(jj++)%128]=pitch
-//console.log(pitch)
+		if (ac == -1) 
+			kk++;	
+		else if (ac < 300) {
+			pitch=Math.round(ac);
+			pitchElem.innerText=pitch;
+			tbuf[(jj++)%512]=pitch;
+			}
 	}
 
+	function updatePitch2() 
+	{
+		var i,y;
+		analyser.getFloatTimeDomainData(buf);
+		var ac=autoCorrelate(buf,audioContext.sampleRate/2);
+		waveCanvas2.clearRect(0,0,512,256);
+		waveCanvas2.strokeStyle="#990000";
+		waveCanvas2.lineWidth=6;
+		waveCanvas2.lineCap="round";
+		waveCanvas2.beginPath();
+		waveCanvas2.moveTo(0,256-tbuf[1]/2);
+		for (var i=1;i<512;i++) {
+			if (tbuf[i] == 0)  break;
+			y=tbuf[i]-0+((tbuf[i+1]-tbuf[i])/2);					// Mid y										
+			waveCanvas2.quadraticCurveTo(i*4,256-tbuf[i]/2,i*4+2,256-y/2);
+			}
+		waveCanvas2.stroke();
 
-}
-else 
-kk++
-
-if (kk > 100)
-kk=jj=0;
-
-	if (!window.requestAnimationFrame)
-		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
-	rafID = window.requestAnimationFrame( updatePitch );
-}
-
+		if (ac == -1) 
+			kk++;	
+		else if (ac < 300) {
+			pitch=Math.round(ac);
+			pitchElem.innerText=pitch;
+			tbuf[(jj++)%512]=pitch;
+			}
+	}
+	
 ////////////////////////////////////////////////////////////////////////////////////////////
 // HELPERS
 ////////////////////////////////////////////////////////////////////////////////////////////
